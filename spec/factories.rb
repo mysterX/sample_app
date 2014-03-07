b593457b68ade303497fb817506fc5530408a308
mysterX <mikes75238@gmail.com>
2014-03-06 22:44:44 -0600

    Finish user signup

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name                  "Elvis Pretzel"
+    email                 "elvis@graceland.com"
+    password              "allshookup"
+    password_confirmation "allshookup"
+  end
+end
